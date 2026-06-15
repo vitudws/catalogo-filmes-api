2e89288b98e508ede2e428623a365c4785a381ee
Vitor Augusto <vitorag150@gmail.com>
2026-06-15 10:24:07 -0300

    Adiciona comentários JavaDoc ao FilmeRepository
    Added Javadoc comment explaining the purpose of the FilmeRepository.

--- src/main/java/com/a3/catalogofilme/repository/FilmeRepository.java
+++ src/main/java/com/a3/catalogofilme/repository/FilmeRepository.java
@@ -3,5 +3,9 @@ package com.a3.catalogofilme.repository;
 import com.a3.catalogofilme.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository responsável pela comunicação com a tabela filmes.
+ * O JpaRepository já fornece métodos prontos como findAll, findById, save e deleteById.
+ */
 public interface FilmeRepository extends JpaRepository<Filme, Integer> {
 }
